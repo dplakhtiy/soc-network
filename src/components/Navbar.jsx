1b7248620ae0a463f7213278e7f4addcd6f82f4b
Denchik <dplakhtiy@ukr.net>
2020-09-16 13:04:28 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = ()=>{
+    return (
+        <nav className="main-menu">
+            <ul>
+            <li><a>menu</a></li>
+            <li><a>menu</a></li>
+            <li><a>menu</a></li>
+            <li><a>menu</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = ()=>{
+    return (
+        <div className="main-content">
+            <div className="img-fill">
+                <img src="https://therightsofnature.org/wp-content/uploads/2018/01/turkey-3048299_1920-1366x550.jpg" alt="nature"/>
+            </div>
+            <div>
+                desk
+            </div>
+            <div className="posts">
+                <div className="new-post"></div>
+                <div>
+                    <div className="post">
+                        post
+                    </div>
+                    <div className="post">
+                        post
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
